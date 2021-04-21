e907602a89cdd6b520ddfa75318946ecef76a7d7
Hutasako <ejjungao@yahoo.com>
2021-04-21 12:25:19 -0400

    Added test for cookbookcli.js and fixed minor bug
    - Fixed minor bug where recipes names were not passed to the cli get and remove commands in cookbook-cli.js.
    
    - Tested for all methods through CLI commands which include adding, deleting, and listing recipes.

--- src/cookbook-cli.js
+++ src/cookbook-cli.js
@@ -4,34 +4,55 @@ class CookbookCli {
   constructor(cookbook) {
     this.cookbook = cookbook;
   }
-
+  
   run(command, ...args) {
     switch (command) {
       case 'list': return this.list();
       case 'add': return this.add(...args);
-      case 'get': return this.get();
-      case 'remove': return this.remove();
+      case 'get': return this.get(...args);
+      case 'remove': return this.remove(...args);
       default: return `Whoops, the following command is unsupported: ${command}.`;
     }
   }
-
+  
   list() {
-    return `You have the following recipes: ${this.cookbook.listRecipes().join(',')}`;
+    // Check if the returned array is empty or not
+    if(this.cookbook.listRecipes().length){
+      return `You have the following recipes: ${this.cookbook.listRecipes().join(',')}`;
+    }
+    else{
+      return `Your recipes are in another cookbook (Empty).`;
+    }
   }
-
+  
   add(name, ingredients) {
-    this.cookbook.addRecipe(name, ingredients);
-    return `Successfully added the following recipe: ${name}`;
+    if(this.cookbook.getRecipe(name)){
+      return 'There can only be one (Duplicate entry).'
+    }
+    else {
+      this.cookbook.addRecipe(name, ingredients);
+      return `Successfully added the following recipe: ${name}`;
+    }
   }
-
+  
   get(name) {
-    return `The ingredients for ${name} are: ${this.cookbook.getRecipe(name)}`;
+    if(this.cookbook.getRecipe(name)){
+      return `The ingredients for ${name} are: ${this.cookbook.getRecipe(name)}`;
+    }
+    else{
+      return `These are not the recipes you are looking for (Does not exist).`;
+    }
   }
-
+  
   remove(name) {
-    this.cookbook.removeRecipe(name);
-    return `Successfully removed the following recipe: ${name}`;
-  }
+    if(this.cookbook.getRecipe(name)){
+      this.cookbook.removeRecipe(name);
+      return `Successfully removed the following recipe: ${name}`;
+    }
+    else {
+      return `I was never here (Cannot delete a recipe that does not exist)`;
+    }
+  } 
 }
 
 module.exports = { CookbookCli };
--- test/cookbook-cli.test.js
+++ test/cookbook-cli.test.js
@@ -2,27 +2,80 @@ const { Cookbook } = require('../src/cookbook');
 const { CookbookCli } = require('../src/cookbook-cli');
 
 describe('CookbookCli', () => {
+  const cBook = new Cookbook();
+  const recipeName = 'Cereal';
+  const recipeIng = ['Milk', 'Cereal'];
+  const cBookCli = new CookbookCli(cBook);
+  
   describe('Adding recipes', () => {
     test('should accept the recipe information and display the correct message', () => {
-
+      
+      const runAdd = cBookCli.run('add', recipeName, recipeIng);
+      
+      expect(runAdd).toBe(`Successfully added the following recipe: ${recipeName}`);
     });
   });
-
+  
+  describe('Adding a duplicate recipe', () => {
+    test('should accept the recipe information and display the correct message', () => {
+      
+      const runAdd = cBookCli.run('add', recipeName, recipeIng);
+      
+      expect(runAdd).toBe('There can only be one (Duplicate entry).');
+    });
+  });
+  
   describe('Listing recipes', () => {
     test('should display the correct message listing all of the recipe names', () => {
-
+      const runList = cBookCli.run('list'); 
+      
+      expect(runList).toBe(`You have the following recipes: ${recipeName}`)
+      
+      
     });
   });
-
+  
+  describe('Listing empty cookbook', () => {
+    test('should display the correct message for an empty cookbook', () => {
+      
+      const emptyBook = new Cookbook();
+      const emptyBookCli = new CookbookCli(emptyBook);
+      const runEmptyList = emptyBookCli.run('list');
+      
+      expect(runEmptyList).toBe(`Your recipes are in another cookbook (Empty).`);
+      
+    })
+  })
+  
   describe('Retrieving a recipe', () => {
     test('should display the ingredients required to make the specified recipe', () => {
-
+      const runGet = cBookCli.run('get', `${recipeName}`);
+      
+      expect(runGet).toBe(`The ingredients for ${recipeName} are: ${recipeIng}`);
+    });
+  });
+  
+  describe('Retrieving a recipe that does not exist', () => {
+    test('should display the ingredients required to make the specified recipe', () => {
+      const runGet = cBookCli.run('get', `Poutine`);
+      
+      expect(runGet).toBe(`These are not the recipes you are looking for (Does not exist).`);
     });
   });
 
   describe('Deleting a recipe', () => {
     test('should accept the recipe name and display the correct message', () => {
+      const runDel = cBookCli.run('remove', `${recipeName}`);
+      
+      expect(runDel).toBe(`Successfully removed the following recipe: ${recipeName}`);
+    });
+  });
 
+  describe('Deleting a recipe that does not exist', () => {
+    test('should accept the recipe name and display the correct message', () => {
+      const runDel = cBookCli.run('remove', `Poutine`);
+      
+      expect(runDel).toBe(`I was never here (Cannot delete a recipe that does not exist)`);
     });
   });
 });
